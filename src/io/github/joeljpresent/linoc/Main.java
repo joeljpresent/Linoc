1f75cfa274b83143864995cac0f52c19da53a1f1
JoelJPresent <36854931+joeljpresent@users.noreply.github.com>
2018-09-15 14:10:25 +0200

    Add bare-boned LinocRules

--- src/io/github/joeljpresent/linoc/Main.java
+++
@@ -1,9 +0,0 @@
-package io.github.joeljpresent.linoc;
-
-public class Main
-{
-    public static void main(String[] args)
-    {
-        
-    }
-}
---
+++ src/io/github/joeljpresent/linoc/swapper/LinocRules.java
@@ -0,0 +1,8 @@
+package io.github.joeljpresent.linoc.swapper;
+
+/**
+ * Correspondence between a template color and the intended color.
+ */
+public class LinocRules
+{
+}
