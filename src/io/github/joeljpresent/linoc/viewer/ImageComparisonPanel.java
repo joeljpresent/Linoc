38bdb9ee4b4b8993411c2597e7e7249e50ddbe35
JoelJPresent <36854931+joeljpresent@users.noreply.github.com>
2018-09-16 17:26:46 +0200

    Implement functioning (yet awful) LinocViewer.

--- src/io/github/joeljpresent/linoc/viewer/ImageComparisonPanel.java
+++ src/io/github/joeljpresent/linoc/viewer/ImageComparisonPanel.java
@@ -1,11 +1,24 @@
 package io.github.joeljpresent.linoc.viewer;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 class ImageComparisonPanel extends JPanel
 {
-    ImageComparisonPanel()
+    final private BufferedImage src;
+    final private BufferedImage dest;
+
+    ImageComparisonPanel(LinocViewer viewer)
     {
-        super();
+        this.src = viewer.getSourceImage();
+        this.dest = viewer.getDestinationImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        g.drawImage(src, 10, 50, 250, 200, this);
+        g.drawImage(dest, 300, 50, 250, 200, this);
     }
 }
